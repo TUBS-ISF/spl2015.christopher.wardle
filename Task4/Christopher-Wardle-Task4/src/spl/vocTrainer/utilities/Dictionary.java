d66b00195f22ddab745ce8b25a452b0099091915
Big Fudge <christopherwardle@yahoo.de>
2015-06-16 15:00:33 +0200

    Adding prepared project for task 4 (nothing new implemented, yet)

---
+++ Task4/Christopher-Wardle-Task4/src/spl/vocTrainer/utilities/Dictionary.java
@@ -0,0 +1,52 @@
+package spl.vocTrainer.utilities;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Dictionary {
+
+	private List<Category> categories;
+	private String name;
+	
+	public Dictionary(String name){
+		setName(name);
+		categories = new ArrayList<Category>();
+	}
+	
+	public String getName() {
+		return name;
+	}
+	
+	private void setName(String name) {
+		this.name = name;
+	}
+
+	public List<Category> getCategories() {
+		return categories;
+	}	
+	
+	public void addCategory(Category c){
+		categories.add(c);
+	}
+	
+	public Category getCategory(int index){
+		return categories.get(index);
+	}
+	
+	public Category getCategory(String name){
+		for(Category c : categories){
+			if(c.getName().equals(name)){
+				return c;
+			}
+		}
+		return null;
+	}
+	
+	public Category getCategory(Category c){
+		return categories.get(categories.indexOf(c));
+	}
+	
+	public int getCategoryIndex(Category c){
+		return categories.indexOf(c);
+	}
+}
---
+++ Task4/Christopher-Wardle-Task4/src/spl/vocTrainer/utilities/Entry.java
@@ -0,0 +1,21 @@
+package spl.vocTrainer.utilities;
+
+public class Entry {
+
+	private String key;
+	private String value;
+	
+	public Entry(String key, String value){
+		this.key = key;
+		this.value = value;
+	}
+	
+	public String getValue(){
+		return this.value;
+	}
+	
+	public String getKey(){
+		return this.key;
+	}
+	
+}
